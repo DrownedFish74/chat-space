5e9bd56abc3a2d21bb8af6840618c291ddf295a1
DrownedFish74 <drowned.lolita@gmail.com>
2020-04-10 17:57:55 +0900

    Fix broken layout

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,42 @@
+$(function(){
+  function buildHTML(message) {
+    var html = `<div class="messages__message">
+                  <div class="messages__message__user">
+                    <div class="messages__message__user__name">
+                      ${message.user.name}
+                    </div>
+                    <div class="messages__message__user__time">
+                      ${message.created_at.strftime("%Y年%m月%d日 %H時%M分")}
+                    </div>
+                  </div>
+                  <div class="messages__message__text">
+                    ${message.text}`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.main-chat__messages').append(html);
+    $('.form__box__top__text').val('');
+    $('.form__box__top__image').val('');
+    $('.form__submit').prop('disabled', false);
+  })
+  .fail(function(){
+    alert('error');
+  })
+})
+})
+
+
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.text @message.text
+json.image @message.image
+json.user @message.user.name
+json.time @message.created_at
