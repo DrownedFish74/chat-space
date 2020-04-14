a3404d11a1de71d4c8696a8bcbf1d43f7ba8e19c
DrownedFish74 <drowned.lolita@gmail.com>
2020-04-14 13:43:17 +0900

    fix minor bugs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.13.0'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:DrownedFish74/.git'
+set :repo_url,  'git@github.com:DrownedFish74/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+  server '18.181.12.34', user: 'ec2-user', roles: %w{app db web}
